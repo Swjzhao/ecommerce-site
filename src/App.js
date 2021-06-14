52c3dec36a1b9e6148bdc43f628af6f4a3fee736
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2021-06-14 15:42:11 -0400

    Minor Styling fixes

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ const App = () => {
 
   return (
     <div>
-
-
       <Router>
         <NavBar />
         <Scaffold>
--- src/components/checkout/styles.js
+++ src/components/checkout/styles.js
@@ -1,6 +1,10 @@
 import {makeStyles} from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+
+  wrapper: {
+    paddingTop: '1px',
+  },
   appBar: {
     position: 'relative',
   },
